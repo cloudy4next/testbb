cbcd5e5b784a6bc8889cd50acd09a9f757345e6e
clouyd4next <jahangir7200@live.com>
2023-02-06 10:16:13 +0600

    query post

--- app/Http/Controllers/Api/ReactApiController.php
+++ app/Http/Controllers/Api/ReactApiController.php
@@ -10,6 +10,8 @@ use App\Models\Page;
 use App\Models\Project;
 use App\Models\News;
 use App\Models\Query;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 
 class ReactApiController extends Controller
 {
@@ -19,7 +21,7 @@ class ReactApiController extends Controller
 
         return response()->json(['success' => $categories], 200);
 
-        if(isset($procategoriesjects))
+        if(empty($procategoriesjects))
         {
             return response()->json(['error' => 'No Resource Found!'], 404);
         }
@@ -30,7 +32,7 @@ class ReactApiController extends Controller
     {
         $notice = Notice::get();
 
-        if(isset($notice))
+        if(empty($notice))
         {
             return response()->json(['error' => 'No Resource Found!'], 404);
         }
@@ -42,43 +44,71 @@ class ReactApiController extends Controller
 
     public function getPages(Request $request)
     {
-    $page = Page::get();
+        $page = Page::get();
 
-    if(isset($page))
-    {
-        return response()->json(['error' => 'No Resource Found!'], 404);
-    }
+        if(empty($page))
+        {
+            return response()->json(['error' => 'No Resource Found!'], 404);
+        }
 
-    return response()->json(['success' => $page], 200);
+        return response()->json(['success' => $page], 200);
 
     }
 
     public function getProjects(Request $request)
     {
 
-    $projects = Project::get();
+        $projects = Project::get();
 
-    if(isset($projects))
-    {
-        return response()->json(['error' => 'No Resource Found!'], 404);
-    }
+        if(empty($projects))
+        {
+            return response()->json(['error' => 'No Resource Found!'], 404);
+        }
 
 
-    return response()->json(['success' => $projects], 200);
+        return response()->json(['success' => $projects], 200);
 
     }
 
     public function getNews(Request $request)
     {
-    $news = News::get();
+        $news = News::get();
 
-    if(isset($news))
-    {
-        return response()->json(['error' => 'No Resource Found!'], 404);
+        if(empty($news))
+        {
+            return response()->json(['error' => 'No Resource Found!'], 404);
+        }
+
+
+        return response()->json(['success' => $news], 200);
+
     }
 
+    public function postQueries(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+        ['name'=> 'required|string|max:255',
+        'email'=> 'required|string|email|max:255',
+        'phone'=> 'required|string|max:15',
+        'title'=> 'required|string|max:255',
+        'description'=> 'required|min:3|max:1000',
+        ]);
 
-    return response()->json(['success' => $news], 200);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $queryStore = new Query();
+        $queryStore->name = $request->name;
+        $queryStore->email = $request->email;
+        $queryStore->phone = $request->phone;
+        $queryStore->title = $request->title;
+        $queryStore->description = $request->description;
+        $queryStore->created_at = Carbon::now();
+        $queryStore->save();
+
+        return response()->json(['success' => 'Data Saved Successfully'], 200);
 
     }
 
