7c600ea031764d5df42e6531d9bc902cbb584752
clouyd4next <jahangir7200@live.com>
2023-04-03 11:43:34 +0600

    relaingment sideabr

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,24 +2,10 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
         {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}"><i class="nav-icon la la-files-o"></i>
-        <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('pptx') }}'><i class="las la-file-powerpoint"></i>
-        Upload PPTX</a></li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('query') }}'><i class='nav-icon las la-comments'></i>
-        Queries</a></li>
 
 @if (backpack_user()->hasPermissionTo('Page'))
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon las la-file'></i>
             Pages</a></li>
-@endif
-
-@if (backpack_user()->hasPermissionTo('Project'))
-    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('project') }}'><i
-                class='nav-icon las la-suitcase'></i>
-            Projects</a></li>
 @endif
 
 @if (backpack_user()->hasPermissionTo('News'))
@@ -39,10 +25,33 @@
             Notices/Circulars</a></li>
 @endif
 
+@if (backpack_user()->hasPermissionTo('Project'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('project') }}'><i
+                class='nav-icon las la-suitcase'></i>
+            Projects</a></li>
+@endif
+
+
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('research') }}'><i class="lab la-researchgate"></i>
+        Research</a></li>
+
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('pptx') }}'><i class="las la-file-powerpoint"></i>
+        Upload PPTX</a></li>
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('query') }}'><i class='nav-icon las la-comments'></i>
+        Queries</a></li>
+
 @if (backpack_user()->hasPermissionTo('Category'))
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon las la-tag'></i>
             Categories</a></li>
 @endif
+
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}"><i class="nav-icon la la-files-o"></i>
+        <span>Media</span></a></li>
+
 
 @if (backpack_user()->hasRole('Super admin') || backpack_user()->hasPermissionTo('Settings'))
     <li class="nav-item nav-dropdown">
@@ -78,5 +87,3 @@
         </ul>
     </li>
 @endif
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('research') }}'><i class='nav-icon la la-question'></i> Research</a></li>
